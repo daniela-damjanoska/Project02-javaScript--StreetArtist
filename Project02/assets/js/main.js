ad9f4f6a11fdf61e3d21a7f9151808e10b789bbf
Daniela Damjanoska <daniela_damjanoska@yahoo.com>
2021-11-01 09:01:49 +0100

    slider-photo design and functionality done

--- Project02/assets/js/main.js
+++ Project02/assets/js/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const makeSliderImg = (slideTrack, src) => {
+    slideTrack.innerHTML += `<div class="slide">
+    <img src="${src}" alt="" />
+    </div>`;
+};
+
+items.forEach(el => {
+    const slideTrackOne = document.querySelector('.slide-track-one'),
+        slideTrackTwo = document.querySelector('.slide-track-two');
+
+    makeSliderImg(slideTrackOne, el.image);
+    makeSliderImg(slideTrackTwo, el.image);
+});
