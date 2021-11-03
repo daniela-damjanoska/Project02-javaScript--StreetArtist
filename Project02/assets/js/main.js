c0329e6cbea54ad03ab7962721107ee2e9126bab
Daniela Damjanoska <daniela_damjanoska@yahoo.com>
2021-11-03 19:51:33 +0100

    visitor page listing fixed - items isPublished rendered

--- Project02/assets/js/main.js
+++ Project02/assets/js/main.js
@@ -2,49 +2,52 @@
 
 makeNavbar('Street ARTist', 'auction');
 
-items.forEach((el, idx) => {
+items.forEach(el => {
     makeSliderImg(document.querySelector('.slide-track-one'), el.image);
     makeSliderImg(document.querySelector('.slide-track-two'), el.image);
+});
 
+const filteredPublished = items.filter(item => item.isPublished === true);
+filteredPublished.forEach((item, idx) => {
     if (window.innerWidth <= 768) {
         if (idx % 2 === 0)
             makeVisitorListingPhotos(
-                el.image,
-                el.artist,
-                el.price,
-                el.title,
-                el.description,
+                item.image,
+                item.artist,
+                item.price,
+                item.title,
+                item.description,
                 'bg-light c-text-primary-default',
                 'c-text-normal bg-primary-default'
             );
         else
             makeVisitorListingPhotos(
-                el.image,
-                el.artist,
-                el.price,
-                el.title,
-                el.description,
+                item.image,
+                item.artist,
+                item.price,
+                item.title,
+                item.description,
                 'bg-primary-default c-text-normal',
                 'c-text-primary-default bg-light'
             );
     } else {
         if (idx === 0 || (idx + 1) % 4 === 0 || (idx + 1) % 4 === 1)
             makeVisitorListingPhotos(
-                el.image,
-                el.artist,
-                el.price,
-                el.title,
-                el.description,
+                item.image,
+                item.artist,
+                item.price,
+                item.title,
+                item.description,
                 'bg-light c-text-primary-default',
                 'c-text-normal bg-primary-default'
             );
         else
             makeVisitorListingPhotos(
-                el.image,
-                el.artist,
-                el.price,
-                el.title,
-                el.description,
+                item.image,
+                item.artist,
+                item.price,
+                item.title,
+                item.description,
                 'bg-primary-default c-text-normal',
                 'c-text-primary-default bg-light'
             );
